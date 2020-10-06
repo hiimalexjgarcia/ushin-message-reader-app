7a209cb2627c5fdf6f15f1d7c9d95689a39a631a
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2020-10-05 21:15:31 -0700

    Moved panel to right.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import { ReactComponent as ChevronRight } from "bootstrap-icons/icons/chevron-right.svg";
+import { ReactComponent as ChevronLeft } from "bootstrap-icons/icons/chevron-left.svg";
 
 import { Swipeable } from "react-swipeable";
 
@@ -42,24 +42,24 @@ function App() {
       </div>
 
       <div className="messagePane">
-	<div className="message" style={{ marginLeft: "2rem", padding: "1rem" }}>
+	<div className="message" style={{ marginRight: "1rem", padding: "1rem" }}>
 	  <h1>Header</h1>
 	  <p>
 	    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus euismod, purus vitae varius lacinia, nibh est mollis metus, in tincidunt lorem ligula id lacus. Nunc venenatis nibh ut mauris egestas, vitae tincidunt turpis viverra. Fusce erat lorem, gravida sed mollis in, tempus at turpis. Praesent in justo bibendum, sagittis mauris vel, sollicitudin nulla. Cras facilisis viverra risus at varius. Sed mauris ligula, imperdiet et dictum ac, pellentesque convallis enim. Cras non consectetur nisi, et aliquet ipsum. Aenean tincidunt aliquet tellus et dignissim. Vestibulum volutpat lectus ut arcu condimentum, vitae laoreet ex bibendum. Nam pharetra mollis interdum. Maecenas venenatis dapibus metus, sed interdum arcu hendrerit in.
 	  </p>
 	</div>
         <Swipeable
-          onSwipedRight={handleShowMessagesPanel}
+          onSwipedLeft={handleShowMessagesPanel}
         >
 	<Button
 	  variant="primary"
 	  size="sm"
-	  className="position-fixed h-100 rounded-0"
+	  className="position-fixed h-100 p-0 rounded-0"
 	  aria-label="Activate messagesPanel"
 	  onClick={handleShowMessagesPanel}
-	  style={{ top: 0, left: 0 }}
+	  style={{ top: 0, right: 0 }}
 	>
-          <ChevronRight
+          <ChevronLeft
             width="1em"
             height="1em"
             viewBox="0 0 16 16"
