07ce7499a2f55afa194368813e6bfadedba27300
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2021-04-07 18:51:49 -0700

    Updating the Screen component position.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,15 +5,3 @@ import App from './App'
 test('renders', () => {
   render(<App />)
 })
-
-test('renders messagesPanel', () => {
-  const { container } = render(<App />)
-  const messagesPanel = container.getElementsByClassName('messagesPanel')[0]
-  expect(messagesPanel).toHaveClass('messagesPanel')
-})
-
-test('renders messagePane', () => {
-  const { container } = render(<App />)
-  const messagePane = container.getElementsByClassName('messagePane')[0]
-  expect(messagePane).toHaveClass('messagePane')
-})
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import './App.css'
+import './App.scss'
 
 import Screen from './Screen'
 
@@ -9,7 +9,6 @@ import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 import Container from 'react-bootstrap/Container'
 import Jumbotron from 'react-bootstrap/Jumbotron'
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { ReactComponent as ChevronLeft } from 'bootstrap-icons/icons/chevron-left.svg'
 
@@ -27,72 +26,73 @@ function App (): any {
     <div className='App'>
 
       <Jumbotron fluid>
-	<Container>
-	  <h1>Contextual UI: Screen</h1>
-	  <p>
-	    This is an example of how to use Screen, a Contextual UI component for visualizing
-            and composing ushin messages.
-	  </p>
-	</Container>
+        <Container>
+          <h1>Contextual UI: Screen</h1>
+          <p>
+            This is an example of how to use Screen, a Contextual UI
+            component for visualizing and composing ushin messages.
+          </p>
+        </Container>
       </Jumbotron>
 
       <Container>
-	<Row>
-	  <Col sm={3}>
-	    <p>
-	      The Screen component contains nine Regions, each of which correspond to the nine ushin "shapes".
-	      Each Region contains any number of Points.
-	    </p>
-	  </Col>
-	  <Col sm={9}>
-	    <div className='messagePane'>
-              <Screen style={{ marginRight: '2rem' }} />
-              <Button
-		variant='primary'
-		size='sm'
-		className='position-absolute h-100 p-0 rounded-0'
-		aria-label='Activate messagesPanel'
-		onClick={handleShowMessagesPanel}
-		style={{ top: 0, right: 0 }}
-		{...swipeableHandlers}
-              >
-		<ChevronLeft
-		  width='1em'
-		  height='1em'
-		  viewBox='0 0 16 16'
-		  className='bi bi-chevron-left'
-		  fill='#fff'
-		/>
-              </Button>
-	    </div>
-	  </Col>
-	</Row>
+        <Row>
+
+          <Col sm={3}>
+            <p>
+              The Screen component contains nine Regions, each of which correspond to the nine ushin "shapes".
+              Each Region contains any number of Points.
+            </p>
+          </Col>
+
+          <Col sm={9}>
+
+            <Screen style={{ marginRight: '2rem' }} />
+
+            <Button
+              variant='primary'
+              size='sm'
+              className='position-absolute h-100 p-0 rounded-0'
+              aria-label='Activate messagesPanel'
+              onClick={handleShowMessagesPanel}
+              style={{ top: 0, right: 0 }}
+              {...swipeableHandlers}
+            >
+              <ChevronLeft
+                width='1em'
+                height='1em'
+                viewBox='0 0 16 16'
+                className='bi bi-chevron-left'
+                fill='#fff'
+              />
+            </Button>
+
+          </Col>
+
+        </Row>
       </Container>
 
-      <div className='messagesPanel'>
-        <Modal
-          size='sm'
-          show={messagesPanelIsOpen}
-          onHide={handleCloseMessagesPanel}
-          dialogClassName='modal-dialog-slideout'
-        >
-          <Modal.Header closeButton>
-            <Modal.Title>Modal title</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            ...
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant='secondary' onClick={handleCloseMessagesPanel}>
-              Close
-            </Button>
-            <Button variant='primary' onClick={handleCloseMessagesPanel}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </div>
-
+      <Modal
+        size='sm'
+        show={messagesPanelIsOpen}
+        onHide={handleCloseMessagesPanel}
+        dialogClassName='modal-dialog-slideout'
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          ...
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant='secondary' onClick={handleCloseMessagesPanel}>
+            Close
+          </Button>
+          <Button variant='primary' onClick={handleCloseMessagesPanel}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
 
     </div>
   )
